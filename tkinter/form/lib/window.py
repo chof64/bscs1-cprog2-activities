2692f24d297da8866cf7f6140f6863dfe0b19d9b
Chad Fernandez <contact@chadfernandez.me>
2023-05-19 23:57:53 +0800

    added form activity

---
+++ tkinter/form/lib/window.py
@@ -0,0 +1,9 @@
+
+def window_geometry(context):
+    window_width = 600
+    window_height = 400
+    screen_width = context.winfo_screenwidth()
+    screen_height = context.winfo_screenheight()
+    x = (screen_width - window_width) // 2
+    y = (screen_height - window_height) // 2
+    return f"{window_width}x{window_height}+{x}+{y}"
